20483ec3496c07cd1458166183c6d2e9866a2ddd
james <imjamespond@gmail.com>
2020-05-26 11:41:43 +0800

    first commit

---
+++ MFCApplication1/Erwin.cpp
@@ -0,0 +1,28 @@
+﻿#include "pch.h"
+#include "Erwin.h"
+
+// 将...erwin\Data Modeler r9目录加入库目录
+#import "EAL.dll"
+
+using namespace SCAPI;
+
+Erwin::Erwin()
+{
+	//HRESULT hr;
+	//CLSID clsid;
+	//hr = CLSIDFromProgID(OLESTR("erwin9.SCAPI"), &clsid);
+
+	ISCApplicationPtr scAppPtr;
+	HRESULT hr;
+	hr = scAppPtr.CreateInstance(__uuidof(SCAPI::Application));
+
+	ISCPropertyBagPtr scBag;
+
+	// Retrieve all of application environment properties in one call
+	scBag = scAppPtr->GetApplicationEnvironment()->GetPropertyBag();
+	// Get an array with categories by using empty string as a category name
+	scBag = scAppPtr->GetApplicationEnvironment()->GetPropertyBag("", "Categories");
+
+	// Get Api Version value Application Api category
+	scBag = scAppPtr->GetApplicationEnvironment()->GetPropertyBag("Application.Api", "Api Version");
+}
